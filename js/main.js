d113ceb1bbf43f9d0d3a42e6a3945cd9d416fbc8
Om Rai <105283886+Omrai629@users.noreply.github.com>
2023-07-26 12:51:34 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing-text", {
+    strings: ["Software Development", "Data Science", "Full Stack Web Development", "UI/UX Design"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
